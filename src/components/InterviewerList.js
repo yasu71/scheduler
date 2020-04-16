e3c93b39ec5c2e0ee17ed6668153ddf310c083c2
yasu71 <yasuyo71@gmail.com>
2020-04-15 23:41:20 -0700

    created InterviewerList component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "./InterviewerListItem";
+
+export default function InterviewerList(props){
+  // console.log(props)
+  const interviewers = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem 
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={props.value && interviewer.id === props.value.id}
+        setInterviewer={()=>props.onChange(interviewer)}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">
+        Interviewer
+      </h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
