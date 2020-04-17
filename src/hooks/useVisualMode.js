4d2b58112de372dc40325238a0f763a2096800ff
yasu71 <yasuyo71@gmail.com>
2020-04-17 13:43:35 -0700

    built hooks and passed tests

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (replace) {
+      history.pop();
+    }
+    setMode(newMode);
+    setHistory([...history, newMode]);
+  }
+  
+  function back() {
+    if(history.length > 1) {
+      setHistory(history.slice(0,-1));
+      setMode(history[history.length-2]);
+    }
+  }
+  
+  return { mode, transition, back};
+}
