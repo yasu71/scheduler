08ae5273bb50095a44530f5a626911779d849b9f
yasu71 <yasuyo71@gmail.com>
2020-04-16 21:27:04 -0700

    implimented getInterview function

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 import "components/Application.scss";
 
@@ -19,10 +19,12 @@ export default function Application(props) {
   useEffect(() => {
     Promise.all([
       axios.get("/api/days"),
-      axios.get("/api/appointments")
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
     .then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
+      console.log(all[2].data)
     })
   }, []);
   
@@ -50,6 +52,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
        {appointments.map(appointment => {
+         getInterview(state, appointment.interview);
           return (
             <Appointment
               key={appointment.id}
