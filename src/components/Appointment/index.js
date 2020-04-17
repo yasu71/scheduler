67d387bd60b01dc140c7f6083681a87ca45ce786
yasu71 <yasuyo71@gmail.com>
2020-04-17 15:01:13 -0700

    added Create form component and custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,28 +1,40 @@
 import "components/Appointment/styles.scss";
-import React, { Fragment } from "react";
+import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
 
-  if(props.interview){
-    return (
-      <article className="appointment">
-        <Header time={props.time} />
-        <Show 
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-        />
-      </article>
-    );
-  }
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      <Empty />
+        {mode === EMPTY && 
+          <Empty onAdd={() => transition(CREATE)}/>
+        }
+        {mode === SHOW && 
+          (<Show 
+            student={props.interview.student}
+            interviewer={props.interview.interviewer}
+          />)
+        }
+        {mode === CREATE && 
+          (<Form 
+            interviewers={[]}
+            onSave={props.save}
+            onCancel={() => back()}
+          />)
+        }
     </article>
   );
 };
