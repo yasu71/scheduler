7f083011f866859b4905be095686ec634a532a53
yasu71 <yasuyo71@gmail.com>
2020-04-16 18:23:28 -0700

    combined state into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,26 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
 import DayList from "components/DayList";
-import Appointment from "components/Appointment"
-import "components/Application.scss";
+import Appointment from "components/Appointment";
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
+import "components/Application.scss";
 
 const appointments = [
   {
@@ -71,37 +54,45 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [daysData, setDays] = useState([]);
-  const [day, setDay] = useState("");
+  // const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("");
+  // const setDays = days => setState(prev => ({ ...prev, days }));
+  const setDay = day => setState({ ...state, day });
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
-  
   useEffect(() => {
-    axios
-      .get("http://localhost:8000/api/days")
-      .then((response) => {
-        setDays(response["data"])
-      })
+    Promise.all([
+      axios.get("/api/days"),
+      axios.get("/api/appointments")
+    ])
+    .then((all) => {
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}))
+    })
   }, []);
-
+  
   return (
     <main className="layout">
       <section className="sidebar">
       <img
         className="sidebar--centered"
-        src="images/logo.png"
+        src="/images/logo.png"
         alt="Interview Scheduler"
       />
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
       <DayList
-        days={daysData}
-        day={day}
+        days={state.days}
+        day={state.day}
         setDay={setDay}
       />
       </nav>
       <img
         className="sidebar__lhl sidebar--centered"
-        src="images/lhl.png"
+        src="/images/lhl.png"
         alt="Lighthouse Labs"
       />
       </section>
