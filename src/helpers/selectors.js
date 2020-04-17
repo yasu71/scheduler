3d57bcadfde1258860f8565c8e990d7c138a4552
yasu71 <yasuyo71@gmail.com>
2020-04-16 21:23:38 -0700

    added getInterview function and tests and passed the tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,36 +1,28 @@
 export function getAppointmentsForDay(state, providedDay) {
   // get the whole day obj
   const dayObj = state.days.find(day => {
-    return day.name === providedDay
+    return day.name === providedDay;
   })
 
   if (!dayObj) {
-    return []
+    return [];
   }
 
   // populate the array with the appointment object
   return dayObj.appointments.map((appointmentID) => {
-    return state.appointments[appointmentID]
+    return state.appointments[appointmentID];
   })
+};
 
-
-  // const filteredDays = state.days.filter(day => {
-  //   const dayAppointments = []
-  //   if (day.name === providedDay){
-  //     for (const appointments of day.appointments) {
-  //       for(const schedule in state.appointments){
-  //         if (appointments === state.appointments[schedule].id) {
-  //           dayAppointments.push(state.appointments[schedule]);
-  //         } else {
-  //           dayAppointments;
-  //         }
-  //       }
-  //     }
-  //     console.log("dayAppointments: ", dayAppointments)
-  //     return dayAppointments;
-  //   }
-  // });
-  // console.log("filteredDays: ",filteredDays)
-  // return filteredDays;
-
-}
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  } else {
+    for (const interviewer in state.interviewers) {
+      if (parseInt(interviewer) === interview.interviewer) {
+        interview.interviewer = state.interviewers[interviewer];
+        return interview;
+      }
+    }
+  }
+};
